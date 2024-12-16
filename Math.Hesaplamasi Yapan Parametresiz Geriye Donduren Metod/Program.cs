dcce460918e1e18a6a3af046e5273cab72251d4d
yaren <yaren@DESKTOP-9U2BG79>
2024-12-17 01:31:00 +0300

    Add project files.

---
+++ Math.Hesaplamasi Yapan Parametresiz Geriye Donduren Metod/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math.Hesaplamasi_Yapan_Parametresiz_Geriye_Donduren_Metod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Üçgenin Taban Uzunluğunu Giriniz: ");
+            double taban = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Üçgenin Yüksekliğini Giriniz: ");
+            double yukseklik = Convert.ToDouble(Console.ReadLine());
+
+            double alan = UcgenAlaniHesapla(taban, yukseklik);
+
+            Console.WriteLine("Üçgenin Alanı: " + alan);
+
+            Console.ReadKey();
+        }
+
+        static double UcgenAlaniHesapla(double taban, double yukseklik)
+        {
+            return (taban * yukseklik) / 2;
+        }
+
+    }
+}
